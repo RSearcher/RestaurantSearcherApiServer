974df73ce565d42ce26087a910ec83ffa8e9c627
Kentaro KAZAMA <kazehara@outlook.com>
2018-03-06 14:44:28 +0900

    Modify Run

--- server/server.go
+++ server/server.go
@@ -3,10 +3,22 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/olivere/elastic"
+	"net/http"
+	"github.com/wantedly/yashima-news-timeline/server/util"
+	"github.com/wantedly/yashima-news-timeline/server"
 )
 
 func Run(r *gin.Engine) {
-	r.Run(":8080")
+	httpServer := &http.Server{
+		Addr: "0.0.0.0:8080",
+		Handler: util.NewMethodOverrider(r),
+		ReadTimeout: server.ServerReadTimeout,
+		WriteTimeout: server.ServerWriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	if err := httpServer.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
 
 func SetupServer(ESClient *elastic.Client) *gin.Engine {
