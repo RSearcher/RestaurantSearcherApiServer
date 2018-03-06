0181b50db957433e73081fa05f4772081eb6d087
Kentaro KAZAMA <kazehara@outlook.com>
2018-03-06 14:46:39 +0900

    Revert "Modify Run"
    This reverts commit 974df73

--- server/server.go
+++ server/server.go
@@ -3,22 +3,10 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/olivere/elastic"
-	"net/http"
-	"github.com/wantedly/yashima-news-timeline/server/util"
-	"github.com/wantedly/yashima-news-timeline/server"
 )
 
 func Run(r *gin.Engine) {
-	httpServer := &http.Server{
-		Addr: "0.0.0.0:8080",
-		Handler: util.NewMethodOverrider(r),
-		ReadTimeout: server.ServerReadTimeout,
-		WriteTimeout: server.ServerWriteTimeout,
-		MaxHeaderBytes: 1 << 20,
-	}
-	if err := httpServer.ListenAndServe(); err != nil {
-		panic(err)
-	}
+	r.Run(":8080")
 }
 
 func SetupServer(ESClient *elastic.Client) *gin.Engine {
