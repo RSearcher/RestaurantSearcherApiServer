12dc4c1593912155000ca6c9af137cb1bbebe6af
Kentaro KAZAMA <kazehara@outlook.com>
2018-03-09 20:41:23 +0900

    Set config as a middleware

--- server/server.go
+++ server/server.go
@@ -3,16 +3,18 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/olivere/elastic"
+	"RestaurantSearcherAPI/config"
 )
 
 func Run(r *gin.Engine) {
 	r.Run(":8080")
 }
 
-func SetupServer(ESClient *elastic.Client) *gin.Engine {
+func SetupServer(conf *config.Config, ESClient *elastic.Client) *gin.Engine {
 	r := gin.Default()
 
 	r.Use(func(c *gin.Context) {
+		c.Set("Config", conf)
 		c.Set("ESClient", ESClient)
 		c.Next()
 	})
