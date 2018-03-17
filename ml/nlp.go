0a6a6d8113af4f527aa4bf852fed69ad86fd89a6
Kentaro KAZAMA <kazehara@outlook.com>
2018-03-18 04:29:15 +0900

    Add SimilarTerms calling

--- ml/nlp.go
+++ ml/nlp.go
@@ -6,7 +6,39 @@ import (
 	"github.com/pkg/errors"
 	"github.com/gin-gonic/gin/json"
 	"bytes"
+	"net/http"
 )
+
+func (c *Client) SimilarTerms(ctx context.Context, query *models.Query) (*models.Terms, error) {
+	spath := "/terms"
+
+	input, err := json.Marshal(query)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := c.newRequest(ctx, "POST", spath, bytes.NewBuffer(input))
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.Wrapf(err, "status code is not 200 but %s", res.StatusCode)
+	}
+
+	var terms models.Terms
+	if err := decodeBody(res, &terms); err != nil {
+		return nil, err
+	}
+
+	return &terms, nil
+}
 
 func (c *Client) ParseKNP(ctx context.Context, review *models.Review) (*models.ParsedText, error) {
 	spath := "/parse"
--- models/models.go
+++ models/models.go
@@ -9,3 +9,11 @@ type ParsedText struct {
 	Candidates []string `json:"candidates"`
 	Morphs [][][]string `json:"morphs"`
 }
+
+type Query struct {
+	Query string `json:"query"`
+}
+
+type Terms struct {
+	Terms []string `json:"terms"`
+}
